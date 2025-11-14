18a30836b059badb45d288e50002d4f27a498d48
sobanahmed099 <sobanahmedkigs125@gmail.com>
2025-11-14 17:38:31 +0500

    Add files via upload

---
+++ Javascript Class 2 Asiignment 14/app.js
@@ -0,0 +1,156 @@
+// var city = prompt("Enter your city name");
+// if (city=="Karachi"){
+//     alert("Welcome to the city of lights");
+// }
+// var gender =prompt("Plaase,Enter your gender")
+// var male = "Male";
+// var female= "Female";
+// if (gender=male){
+//     alert("Good Morning Sir");
+// } 
+// else{
+//     alert("Good Morning Ma'am");
+// }
+
+
+
+
+
+// var color = prompt("Enter traffic signal color (Red, Yellow, or Green):");
+// var Red= "red"
+// var Yellow ="yellow"
+// var Green= "green"
+// if(color==Red){
+//     document.write("Red = Must Stop <br>")
+//     document.write("Yellow = Ready To Move Now <br>")
+//     document.write("Green = Move Now <br>")
+// }
+// if(color==Green){
+//     document.write("Green = Move Now <br>")
+//     document.write("Red = Must Stop <br>")
+//     document.write("Yellow = Ready To Move Now <br>")
+// }
+
+// if(color==Yellow){
+//     document.write("Yellow = Ready To Move Now <br>")
+//     document.write("Red = Must Stop <br>")
+//     document.write("Green = Move Now <br>")
+// }
+
+// var fuel = prompt("Enter your remaining fuel in liters")
+// if(fuel <= 0.25){
+//     alert("Please refill the fuel in your car")
+// }
+// else{
+//     alert("ohh,Great")
+// }
+
+
+// var a= 4;
+// if (++a===5){
+//     alert("Given condition for variable a is true")
+// }
+// var b= 82;
+// if (b++===83){
+//     alert("Given condition for variable b is true")
+// }
+// var c= 12;
+// if (c++===13){
+//     alert("Given condition for variable c is true")
+// }
+// if (c===13){
+//     alert(" condition 2 for variable c is true")
+// }
+// if (++c>14){
+//     alert(" condition 3 for variable c is true")
+// }
+// if (c===14){
+//     alert(" condition 4 for variable c is true")
+// }
+
+// var materialCost =20000;
+// var laboueCost = 2000;
+// var totalCost = materialCost +laboueCost;
+// if(totalCost===materialCost +laboueCost){
+//     alert("The cost equals")
+// }
+// if(true){
+//     alert("True")
+// }
+// if(false){
+//     alert("False")
+// }
+
+
+
+// var totalMarks = prompt("Enter Your Total Marks")
+// document.write("Total Marks : "  +totalMarks +"<br>" )
+// var marksObtained =prompt("Enter How Much You Obtained")
+// document.write("Obtained Marks:"+marksObtained+"<br>")
+// var per = (marksObtained/totalMarks)*100
+// document.write("Percentage :"+per +"%<br>")
+
+// if(per = 80 | per <= 80){
+//     document.write("Grade: A-One <br>")
+//     document.write("Remark: Excellent <br>")
+// }
+// else{
+//     document.write("")
+// }
+// if(per = 70 | per <= 60 ){
+//     document.write("Grade: B <br>")
+//     document.write("Remark:Good <br>")
+// }
+
+
+
+
+
+
+
+// var a = 9
+// var b = +prompt("Guess a number between 1 to 10")
+// if (b==9){
+//     alert("bingo, Correct Answer")
+// }
+
+// if (b ==10 || b ==8){
+//     alert("Close enough to the correct answer")
+// }
+
+// var x =+prompt("Enter a number between 1 to 60 and check, It is divible by 3 or not.")
+// var y = x/3
+// if ( x == 3 || x == 6 ||x == 9 || x == 12|| x == 15 || x ==18 || x ==  21 || x ==24 ||x ==27|| x ==30 || x ==33 || x ==36 || x ==39 || x ==42 || x ==45 || x ==48 || x ==51 || x ==54 || x ==57 || x ==60 ){
+//     alert("It is divisible by 3")
+// }
+// else{
+//     alert("It is not divisible by 3")
+// }
+
+// var z=+prompt("Enter a number between 1 till 50 and check wheater the number is even or odd")
+// if(z==2 || z==4 || z==6 || z==8 || z==10 || z==12 || z==14 || z==16 || z==18 || z==20 || z==22 || z==24 || z==26 || z==28 || z==30 || z==32 || z==34 || z==36 || z==38 || z==40 || z==42 ||z==44 || z==46 || z==48 || z == 50 ){
+//     alert("It ia a  Even Number")
+// }
+// else{
+//     alert("It is a Odd Number")
+// }
+
+var temp 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
